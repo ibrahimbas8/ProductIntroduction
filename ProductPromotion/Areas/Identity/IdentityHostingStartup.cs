624dfe935c388066574e7dcc3b14cb8c3e6d55de
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 01:52:07 +0300

    Identity module added

---
+++ ProductPromotion/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(ProductPromotion.Areas.Identity.IdentityHostingStartup))]
+namespace ProductPromotion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
