4bf3c87fdaf6b61bfcac6486ee04971124a9d983
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 00:24:53 +0300

    Context added with EF.Core packages

---
+++ ProductPromotion/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductPromotion.Entities;
+using System.Collections.Generic;
+
+namespace ProductPromotion.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
