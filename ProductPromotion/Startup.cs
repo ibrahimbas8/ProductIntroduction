abc41a304db276c543121cae056992e344994520
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 00:29:32 +0300

    Startup class configured

--- ProductPromotion/Startup.cs
+++ ProductPromotion/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductPromotion.Data;
+using ProductPromotion.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +25,12 @@ namespace ProductPromotion
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            // add database dependecy
+            services.AddDbContext<Context>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("ProductDbConnection")));
+            // add repository dependecy
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddRazorPages();
         }
 
