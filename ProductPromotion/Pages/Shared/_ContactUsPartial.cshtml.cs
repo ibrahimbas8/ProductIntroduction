8b0f7df08fa020d68f90a2ece8f415408d0c9397
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 12:46:17 +0300

    _ContactUsPartial

---
+++ ProductPromotion/Pages/Shared/_ContactUsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProductPromotion.Pages.Shared
+{
+    public class _ContactUsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
