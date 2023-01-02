4e7c944ffe016b38106f026de7d50a4e163631cf
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 15:49:08 +0300

    cart entities added

---
+++ ProductPromotion/Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductPromotion.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
