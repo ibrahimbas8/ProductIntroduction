07dcfe679a71987548e30de396c3bca077ab60a2
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 15:50:01 +0300

    cart context added

--- ProductPromotion/Data/Context.cs
+++ ProductPromotion/Data/Context.cs
@@ -16,5 +16,8 @@ namespace ProductPromotion.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
