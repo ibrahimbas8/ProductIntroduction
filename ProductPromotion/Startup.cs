a97eecc2ac76a79322405733a676895ff170dff0
İbrahim Baş <bas.ibrahim98@gmail.com>
2023-01-02 13:53:45 +0300

    removed Identity configure class

--- ProductPromotion/Startup.cs
+++ ProductPromotion/Startup.cs
@@ -1,16 +1,18 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProductPromotion.Data;
 using ProductPromotion.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProductPromotion
 {
@@ -25,15 +27,61 @@ namespace ProductPromotion
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        { 
-            // add database dependecy
+        {
+            ConfigureAspnetRunServices(services);
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddRazorPages();
+
+            services.AddMvc();
+        }
+
+        private void ConfigureAspnetRunServices(IServiceCollection services)
+        {
+            #region database services
+
+            // use in-memory database
             services.AddDbContext<Context>(c =>
                 c.UseSqlServer(Configuration.GetConnectionString("ProductDbConnection")));
+
+            //// add database dependecy
+            //services.AddDbContext<AspnetRunContext>(c =>
+            //    c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection")));
+
+            #endregion
+
+            #region identity services
+
+            services.AddDefaultIdentity<IdentityUser>()
+                 //.AddDefaultUI(UIFramework.Bootstrap4)
+                 .AddEntityFrameworkStores<Context>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
+            #endregion
+
+            #region project services
+
             // add repository dependecy
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IContactRepository, ContactRepository>();
 
-            services.AddRazorPages();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
